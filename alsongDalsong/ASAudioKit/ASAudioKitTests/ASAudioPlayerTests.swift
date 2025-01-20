36cdd2597ec2231f797de1574977d5d9e4642a23
hyunjuntyler <hyunjun1994@hanyang.ac.kr>
2025-01-20 18:00:36 +0900

    [refactor] ASAudioPlayerTests 빌드 가능하게 수정

--- alsongDalsong/ASAudioKit/ASAudioKitTests/ASAudioPlayerTests.swift
+++ alsongDalsong/ASAudioKit/ASAudioKitTests/ASAudioPlayerTests.swift
@@ -2,49 +2,31 @@ import Foundation
 import Testing
 
 struct ASAudioPlayerTests {
-    var player: ASAudioPlayer?
-    var testMusic: Data?
+    private let player: ASAudioPlayer
+    private let testMusic: Data
 
     init() async throws {
         player = ASAudioPlayer()
-        if let url = Bundle.main.url(forResource: "PlayTestDrum", withExtension: "wav") {
-            do {
-                testMusic = try Data(contentsOf: url)
-            } catch {
-                #expect(false)
-            }
-        }
+        let bundle = Bundle(identifier: "kr.codesquad.boostcamp9.alsongDalsong.ASAudioKitTests")
+        let url = try #require(bundle?.url(forResource: "PlayTestDrum", withExtension: "wav"))
+        testMusic = try Data(contentsOf: url)
     }
 
-    @Test func sta$rtPlaying_성공() async throws {
-        guard let player,
-              let testMusic else
-        {
-            #expect(false)
-            return
-        }
+    @Test("재생 시작 성공") func startPlaying() async throws {
+        await player.startPlaying(data: testMusic, option: .full)
 
-        player.startPlaying(data: testMusic, option: .full)
-
-        #expect(player.isPlaying())
+        #expect(await player.isPlaying())
     }
 
-    @Test func 제한된시간동안startPlaying_성공() async throws {
-        guard let player,
-              let testMusic else
-        {
-            #expect(false)
-            return
-        }
-
+    @Test("제한된 시간 동안 재생 시작 성공") func startPlayingWithTimeLimit() async {
         await player.startPlaying(data: testMusic, option: .partial(time: 6))
 
         DispatchQueue.main.asyncAfter(deadline: .now() + 6.0) {
-            #expect(player.isPlaying() == false)
+            Task {
+                #expect(await player.isPlaying() == false)
+            }
         }
-
-        // swiftTesting에서 XCTest에서 비동기 테스트에 사용되는 expectation을 대체할 방법을 못찾겠음
-        // + 기존 XCTAssertEqual에서 오차값을 설정할 수 있었는데 그런게 보이지 않음.
-        #expect(player.isPlaying())
+        
+        #expect(await player.isPlaying())
     }
 }
--- alsongDalsong/ASAudioKit/ASAudioKitTests/ASAudioRecorderTests.swift
+++ alsongDalsong/ASAudioKit/ASAudioKitTests/ASAudioRecorderTests.swift
@@ -2,32 +2,27 @@ import Foundation
 import Testing
 
 struct ASAudioRecorderTests {
-    var recorder: ASAudioRecorder?
+    private let recorder: ASAudioRecorder
 
     init() async throws {
         recorder = ASAudioRecorder()
     }
 
-    @Test func startRecording_성공() async throws {
+    @Test("녹음 시작 테스트") func startRecording() async throws {
         let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
             .appendingPathComponent("녹음성공테스트")
-        guard let recorder else {
-            #expect(false)
-            return
-        }
-        recorder.startRecording(url: url)
-        #expect(recorder.isRecording())
+        
+        await recorder.startRecording(url: url)
+        let isRecording = await recorder.isRecording()
+        #expect(isRecording)
     }
 
-    @Test func stopRecording_성공() async throws {
+    @Test("녹음 완료 후 저장 테스트") func stopRecording() async throws {
         let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
             .appendingPathComponent("녹음저장성공테스트")
-        guard let recorder else {
-            #expect(false)
-            return
-        }
-        recorder.startRecording(url: url)
-        recorder.stopRecording()
+        
+        await recorder.startRecording(url: url)
+        await recorder.stopRecording()
         #expect(FileManager.default.fileExists(atPath: url.path))
     }
 }
