5dd633c3856dfcb36a470773f9c4dad20304b68b
hyunjuntyler <hyunjun1994@hanyang.ac.kr>
2025-01-20 19:45:54 +0900

    [refactor] ASNetworkKit 빌드 가능하게 수정

--- alsongDalsong/ASNetworkKit/ASNetworkKit/Protocols/URLSessionProtocol.swift
+++ alsongDalsong/ASNetworkKit/ASNetworkKit/Protocols/URLSessionProtocol.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-protocol URLSessionProtocol: Sendable {
+public protocol URLSessionProtocol: Sendable {
     func data(for request: URLRequest) async throws -> (Data, URLResponse)
 }
 
