42f1506064a51c26a7214b0f61cb82801edd0a17
hyunjuntyler <hyunjun1994@hanyang.ac.kr>
2025-01-20 18:23:03 +0900

    [refactor] ASCacheKitTests 빌드 가능하게 수정

--- alsongDalsong/ASCacheKit/ASCacheKitTests/ASCacheKitTests.swift
+++ alsongDalsong/ASCacheKit/ASCacheKitTests/ASCacheKitTests.swift
@@ -4,39 +4,40 @@ import Testing
 import UIKit
 
 struct ASCacheKitTests {
-    private var cacheManager = ASCacheManager()
-    let testData = UIImage(systemName: "star")!.pngData()!
-    let testImageURL = URL(string: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUcmUojj8oZ0EzJU027Pul8SpM6ZMxr8HXAgsuunxkFZKSW7K27kLqcsoRaWaEX03kmQg&usqp=CAU")!
-
-    @Test func 디스크에_없는_캐시_로딩() async throws {
-        let cachedData = await cacheManager.loadCache(from: testImageURL, cacheOption: .onlyDisk)
-        let diskData = cacheManager.diskCache.getData(forKey: testImageURL.absoluteString)
-        let memoryData = cacheManager.memoryCache.getObject(forKey: testImageURL.absoluteString)
-
-        #expect(cachedData == nil)
-        #expect(diskData == nil)
-        #expect(memoryData == nil)
+    private let cacheManager: ASCacheManager
+    private let testData: Data
+    private let testImageURL: URL
+    
+    init() throws {
+        cacheManager = ASCacheManager()
+        testData = try #require(UIImage(systemName: "star")?.pngData())
+        testImageURL = try #require(URL(string: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUcmUojj8oZ0EzJU027Pul8SpM6ZMxr8HXAgsuunxkFZKSW7K27kLqcsoRaWaEX03kmQg&usqp=CAU"))
     }
 
-    @Test func 디스크에_있는_캐시_로딩() async throws {
+    @Test("디스크에 없는 캐시 로딩") func loadNonExistingCacheFromDisk() async throws {
+        let diskData = cacheManager.diskCache.getData(forKey: "url")
+
+        #expect(diskData == nil)
+    }
+
+    @Test("디스크에 있는 캐시 로딩") func loadExistingCacheFromDisk() async throws {
         cacheManager.saveCache(withKey: testImageURL, data: testData, cacheOption: .onlyDisk)
-        let cachedData = await cacheManager.loadCache(from: testImageURL, cacheOption: .onlyDisk)
+        let cachedData = cacheManager.loadCache(from: testImageURL, cacheOption: .onlyDisk)
         let diskData = cacheManager.diskCache.getData(forKey: testImageURL.absoluteString)
 
         #expect(cachedData != nil)
         #expect(diskData != nil)
     }
 
-    @Test func 메모리에_없는_캐시_로딩() async throws {
-        cacheManager.saveCache(withKey: testImageURL, data: testData, cacheOption: .onlyDisk)
-        let memoryData = cacheManager.memoryCache.getObject(forKey: testImageURL.absoluteString)
+    @Test("메모리에 없는 캐시 로딩") func loadNonExistingCacheFromMemory() async throws {
+        let memoryData = cacheManager.memoryCache.getObject(forKey: "url")
 
         #expect(memoryData == nil)
     }
 
-    @Test func 메모리에_있는_캐시_로딩() async throws {
+    @Test("메모리에 있는 캐시 로딩") func loadExistingCacheFromMemory() async throws {
         cacheManager.saveCache(withKey: testImageURL, data: testData, cacheOption: .onlyMemory)
-        let cachedData = await cacheManager.loadCache(from: testImageURL, cacheOption: .onlyMemory)
+        let cachedData = cacheManager.loadCache(from: testImageURL, cacheOption: .onlyMemory)
         let memoryData = cacheManager.memoryCache.getObject(forKey: testImageURL.absoluteString)
 
         #expect(cachedData != nil)
