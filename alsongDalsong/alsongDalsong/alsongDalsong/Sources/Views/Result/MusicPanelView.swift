f1d5179cc57f8f85674e33cb63d30e39703310c2
In Ye, Kim <ide7270@naver.com>
2025-01-20 16:51:53 +0900

    [refactor] 폰트 적용안된 부분 적용

--- alsongDalsong/alsongDalsong/alsongDalsong/Sources/Views/Result/MusicPanelView.swift
+++ alsongDalsong/alsongDalsong/alsongDalsong/Sources/Views/Result/MusicPanelView.swift
@@ -56,12 +56,12 @@ final class MusicPanelView: UIView {
         albumImageView.backgroundColor = .secondarySystemBackground
         addSubview(albumImageView)
 
-        musicNameLabel.font = .font(ofSize: 24)
+        musicNameLabel.font = .font(.wantedSansBold, ofSize: 24)
         musicNameLabel.textColor = .asBlack
         musicNameLabel.translatesAutoresizingMaskIntoConstraints = false
         addSubview(musicNameLabel)
 
-        singerNameLabel.font = .font(ofSize: 24)
+        singerNameLabel.font = .font(.wantedSansBold, ofSize: 24)
         singerNameLabel.textColor = UIColor.gray
         singerNameLabel.translatesAutoresizingMaskIntoConstraints = false
         addSubview(singerNameLabel)
