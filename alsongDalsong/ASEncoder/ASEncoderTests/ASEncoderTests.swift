c115d59fef891947fa5e51f2931b7c3fee34e5c7
hyunjuntyler <hyunjun1994@hanyang.ac.kr>
2025-01-20 18:49:53 +0900

    [chore] ASEncoderTests 수정

--- alsongDalsong/ASEncoder/ASEncoderTests/ASEncoderTests.swift
+++ alsongDalsong/ASEncoder/ASEncoderTests/ASEncoderTests.swift
@@ -9,18 +9,17 @@ struct ASEncoderTests {
         let birthDate: Date
     }
 
-    @Test func testmodel_인코딩() async throws {
+    @Test("인코딩 성공") func encode() async throws {
         let dateFormatter = ISO8601DateFormatter()
         let dateString = "2000-01-01T00:00:00Z"
-        let date = dateFormatter.date(from: dateString)!
+        let date = try #require(dateFormatter.date(from: dateString))
+        
         let testModel = TestModel(name: "아이유", age: 30, birthDate: date)
+        
         let jsonData = try ASEncoder.encode(testModel)
-        #expect(jsonData != nil)
-
         let json = try JSONSerialization.jsonObject(with: jsonData, options: [])
-        #expect(json is [String: Any])
 
-        let dictionary = json as! [String: Any]
+        let dictionary = try #require(json as? [String: Any])
         #expect(dictionary["name"] as? String == "아이유")
         #expect(dictionary["age"] as? Int == 30)
         #expect(dictionary["birthDate"] as? String == dateString)
