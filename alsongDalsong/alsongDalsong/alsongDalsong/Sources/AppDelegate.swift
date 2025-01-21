4be9040e577c9a44eab80876a1889121e54577d5
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2025-01-21 21:04:37 +0900

    [Refactor] App Delegate로 Configure 이동
    필요한 Configure 정보들을 App Delegate에서 관리하도록 이동 (SwiftUI의 Preview 때문)

--- alsongDalsong/alsongDalsong/alsongDalsong/Sources/AppDelegate.swift
+++ alsongDalsong/alsongDalsong/alsongDalsong/Sources/AppDelegate.swift
@@ -1,12 +1,23 @@
 import UIKit
+import FirebaseCore
+import ASContainer
+import ASRepository
+import ASNetworkKit
+import ASCacheKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        assembleDependencies()
         return true
     }
 
     func application(_: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options _: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+    
+    private func assembleDependencies() {
+        DIContainer.shared.addAssemblies([CacheAssembly(), NetworkAssembly(), RepsotioryAssembly()])
     }
 }
