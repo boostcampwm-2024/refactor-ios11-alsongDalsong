9c9dca0513670898362db63be65f08e7ecde7ca7
hyunjuntyler <hyunjun1994@hanyang.ac.kr>
2025-02-04 21:57:12 +0900

    [Chore] Firebase 적용 및 권한 요청 설정

--- alsongDalsong/ASAIKit/ASAIKitDemo/Sources/ASAIKitDemoApp.swift
+++ alsongDalsong/ASAIKit/ASAIKitDemo/Sources/ASAIKitDemoApp.swift
@@ -1,7 +1,12 @@
+import FirebaseCore
 import SwiftUI
 
 @main
 struct ASAIKitDemoApp: App {
+    init() {
+      FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ASAIKitDemoView()
