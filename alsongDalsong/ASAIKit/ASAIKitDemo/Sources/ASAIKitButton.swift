f3180581839ea10310289298c3acd69387bae72c
hyunjuntyler <hyunjun1994@hanyang.ac.kr>
2025-02-04 21:56:49 +0900

    [Feat] 커스텀 버튼 추가

---
+++ alsongDalsong/ASAIKit/ASAIKitDemo/Sources/ASAIKitButton.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+
+struct ASAIKitButton: View {
+    var title: String
+    var fontSize: CGFloat = 24
+    var color: Color
+    var isSelected: Bool = true
+    var isDisabled: Bool = false
+    
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .font(.custom("DoHyeon-Regular", size: fontSize))
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background {
+                    RoundedRectangle(cornerRadius: 12, style: .continuous)
+                        .stroke(lineWidth: 4)
+                }
+                .background(isDisabled ? Color(.systemGray5) : isSelected ? color : Color(.systemGray6))
+                .cornerRadius(12)
+                .background {
+                    
+                }
+        }
+        .disabled(isDisabled)
+        .buttonStyle(ASAIKitButtonStyle())
+    }
+}
+
+struct ASAIKitButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .scaleEffect(configuration.isPressed ? 0.97 : 1)
+    }
+}
+
+#Preview {
+    ASAIKitButton(title: "버튼", fontSize: 24, color: .orange) { }
+        .padding()
+}
