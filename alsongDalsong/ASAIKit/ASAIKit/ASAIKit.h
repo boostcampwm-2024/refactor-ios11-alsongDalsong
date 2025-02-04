693a565e9c550f76589c88a0efeabadb4ff9ae97
hyunjuntyler <hyunjun1994@hanyang.ac.kr>
2025-02-04 16:45:54 +0900

    [Feat] ASAIKit 모듈 추가

---
+++ alsongDalsong/ASAIKit/ASAIKit/ASAIKit.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for ASAIKit.
+FOUNDATION_EXPORT double ASAIKitVersionNumber;
+
+//! Project version string for ASAIKit.
+FOUNDATION_EXPORT const unsigned char ASAIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ASAIKit/PublicHeader.h>
+
+
---
+++ alsongDalsong/ASAIKit/ASAIKitDemo/ASAIKitDemoApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct ASAIKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ASAIKitDemoView()
+        }
+    }
+}
