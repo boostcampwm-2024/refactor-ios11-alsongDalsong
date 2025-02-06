b51beaa3e804e7217f17a2ea25c18b647be34d13
around-forest <kleedo19@naver.com>
2025-02-06 14:29:50 +0900

    [Feat] TutorialGuideView와 type 구현

---
+++ alsongDalsong/alsongDalsong/alsongDalsong/Sources/Views/Tutorial/TutorialGuideViewController.swift
@@ -0,0 +1,214 @@
+import ASContainer
+import ASEntity
+import ASRepositoryProtocol
+import UIKit
+
+final class TutorialGuideViewController: UIViewController {
+    private let type: TutorialViewType
+    private let titleLabel = GuideLabel(style: .largeTitle)
+    private let descriptionLabel = GuideLabel(style: .title2)
+    private let cautionLabel = GuideLabel(style: .callout)
+    private var imageContainerView: GuideIconView?
+    private let topButton = ASButton()
+    private let bottomButton = ASButton()
+
+    private let avatars: [URL]?
+    private let selectedAvatar: URL?
+    private let avatarData: Data?
+    private let inviteCode: String?
+
+    init(type: TutorialViewType) {
+        self.type = type
+        self.avatars = nil
+        self.selectedAvatar = nil
+        self.avatarData = nil
+        self.inviteCode = nil
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    init(type: TutorialViewType, avatars: [URL], selectedAvatar: URL, avatarData: Data, inviteCode: String?) {
+        self.type = type
+        self.avatars = avatars
+        self.selectedAvatar = selectedAvatar
+        self.avatarData = avatarData
+        self.inviteCode = inviteCode
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        setupLayout()
+        setupImageView()
+        setAction()
+    }
+
+    private func setupUI() {
+        self.navigationController?.navigationBar.isHidden = true
+        view.backgroundColor = .asLightGray
+        titleLabel.text = type.title.localized()
+        descriptionLabel.text = type.description.localized()
+        cautionLabel.isHidden = true
+
+        if let caution = type.caution {
+            cautionLabel.isHidden = false
+            cautionLabel.text = "* " + caution.localized()
+            cautionLabel.textColor = .systemRed
+        }
+
+        if let symbol = type.symbol {
+            let image = UIImage(systemName: symbol.systemName)
+            let backgroundColor = UIColor(hex: symbol.color)
+            let corneredImageView = GuideIconView(
+                image: image,
+                backgroundColor: backgroundColor
+            )
+            corneredImageView.translatesAutoresizingMaskIntoConstraints = false
+            imageContainerView = corneredImageView
+        }
+
+        topButton.setConfiguration(
+            systemImageName: type.topButton.imageName,
+            text: type.topButton.text.localized(),
+            backgroundColor: UIColor(named: type.topButton.backgroundColor)
+        )
+
+        bottomButton.setConfiguration(
+            systemImageName: type.bottomButton.imageName,
+            text: type.bottomButton.text.localized(),
+            backgroundColor: UIColor(named: type.bottomButton.backgroundColor)
+        )
+
+        topButton.isHidden = type.topButton.isHidden
+        bottomButton.isHidden = type.bottomButton.isHidden
+    }
+
+    private func setupLayout() {
+        view.addSubview(titleLabel)
+        view.addSubview(descriptionLabel)
+        view.addSubview(cautionLabel)
+        view.addSubview(topButton)
+        view.addSubview(bottomButton)
+
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        cautionLabel.translatesAutoresizingMaskIntoConstraints = false
+        topButton.translatesAutoresizingMaskIntoConstraints = false
+        bottomButton.translatesAutoresizingMaskIntoConstraints = false
+
+        let safeArea = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 272),
+            titleLabel.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor),
+
+            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 48),
+            descriptionLabel.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16),
+            descriptionLabel.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -16),
+
+            cautionLabel.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 8),
+            cautionLabel.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16),
+            cautionLabel.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -16),
+
+            topButton.bottomAnchor.constraint(equalTo: bottomButton.topAnchor, constant: -25),
+            topButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+            topButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
+            topButton.heightAnchor.constraint(equalToConstant: 64),
+
+            bottomButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+            bottomButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
+            bottomButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            bottomButton.heightAnchor.constraint(equalToConstant: 64),
+        ])
+    }
+
+    private func setupImageView(){
+        if let imageContainerView {
+            view.addSubview(imageContainerView)
+            NSLayoutConstraint.activate([
+                imageContainerView.widthAnchor.constraint(equalToConstant: 32),
+                imageContainerView.heightAnchor.constraint(equalToConstant: 32),
+                imageContainerView.bottomAnchor.constraint(equalTo: titleLabel.topAnchor,constant: -4),
+                imageContainerView.leadingAnchor.constraint(equalTo: titleLabel.trailingAnchor,constant: 4),
+            ])
+        }
+    }
+    // TODO: -
+    private func setAction() {
+        topButton.addAction(UIAction { [weak self] _ in
+            guard let self else { return }
+            imageContainerView?.animateBounces()
+
+            switch type {
+                case .lobby:
+                    navigateToLobby()
+                default:
+                    return
+            }
+        }, for: .touchUpInside)
+
+        bottomButton.addAction(UIAction { [weak self] _ in
+            guard let self else { return }
+            imageContainerView?.animateBounces()
+
+            switch type {
+                case .lobby:
+                    navigateToOnboarding()
+                case .selectMusic:
+                    return
+                case .humming:
+                    return
+                case .submitAnswer:
+                    return
+                case .result:
+                    return
+                case .finished:
+                    navigateToOnboarding()
+                default:
+                    return
+            }
+        }, for: .touchUpInside)
+    }
+}
+
+private extension TutorialGuideViewController {
+    func navigateToOnboarding() {
+        guard let avatars,
+              let selectedAvatar,
+              let avatarData,
+              let inviteCode
+        else { return }
+
+        let roomActionRepository = DIContainer.shared.resolve(RoomActionRepositoryProtocol.self)
+        let dataDownloadRepository = DIContainer.shared.resolve(DataDownloadRepositoryProtocol.self)
+
+        let onboardingVM = OnboardingViewModel(
+            roomActionRepository: roomActionRepository,
+            dataDownloadRepository: dataDownloadRepository,
+            avatars: avatars,
+            selectedAvatar: selectedAvatar,
+            avatarData: avatarData
+        )
+        let onboardingVC = OnboardingViewController(
+            viewModel: onboardingVM,
+            inviteCode: inviteCode
+        )
+        let navigationController = UINavigationController(rootViewController: onboardingVC)
+        navigationController.navigationBar.isHidden = true
+        navigationController.interactivePopGestureRecognizer?.isEnabled = false
+
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let window = windowScene.windows.first {
+            window.rootViewController = navigationController
+            window.makeKeyAndVisible()
+        }
+    }
+
+    func navigateToLobby() {
+
+    }
+}
---
+++ alsongDalsong/alsongDalsong/alsongDalsong/Sources/Views/Tutorial/TutorialState.swift
@@ -0,0 +1,139 @@
+public enum TutorialState {
+    case start
+}
+
+public enum TutorialViewType {
+    case lobby
+    case selectMusic
+    case humming
+    case submitAnswer
+    case result
+    case finished
+
+    public var title: String {
+        switch self {
+            case .lobby:
+                "튜토리얼"
+            case .selectMusic:
+                "노래 선택"
+            case .humming:
+                "허밍"
+            case .submitAnswer:
+                "정답 맞추기"
+            case .result:
+                "게임 종료!"
+            case .finished:
+                "완료"
+        }
+    }
+
+    public var description: String {
+        switch self {
+            case .lobby: "알쏭달쏭에 오신걸 환영합니다~🎉"
+            case .selectMusic: "문제로 제출할 노래를 고르세요."
+            case .humming: "다음 사람에게 고른 노래를 전달하세요."
+            case .submitAnswer: "허밍을 듣고 무슨 노래인지 맞춰 보세요."
+            case .result: "결과 화면으로 이동합니다."
+            case .finished: "축하합니다! 모든 단계를 마쳤습니다~🎉"
+        }
+    }
+
+    public var caution: String? {
+        switch self {
+            case .lobby:
+                """
+                이곳에서 여러분의 허밍은 곧 음악이 됩니다.
+                
+                간단한 연습을 통해 기본 컨트롤과 플레이 방식을 익히고
+                친구들과 창의적인 음악 여정을 시작해 보세요.
+                
+                아래의 시작 버튼을 눌러 우리의 첫 허밍에 도전해볼까요?
+                """
+            case .selectMusic:
+                """
+                이제 여러분이 친구들에게 문제로 제출할 노래를 고릅니다.
+                
+                제출 전까지 선택을 바꿀 수는 있지만
+                한번 제출을 완료하면 다시 바꿀 수 없으니 주의하세요!
+                
+                또한 선택한 노래를 허밍으로 불러야 한다는 사실도 잊지마세요!
+                
+                그럼 이제 노래를 고르러 가볼까요?
+                """
+            case .humming:
+                """
+                자 드디어 여러분의 목소리를 음악으로 만들 시간입니다.
+                
+                여러분이 녹음한 허밍을 친구들이 듣고 어떤 노래인지 맞추게 되니
+                제목이나 가사를 힌트로 주는 건 신중해야겠죠?!
+                
+                참고로 노래의 어느 부분을 불러도 상관 없답니다!
+                
+                물도 마시고 목소리도 가다듬었다면 시작해볼까요?
+                """
+            case .submitAnswer:
+                """
+                내 허밍 제출 후에는 친구의 허밍을 듣고 노래를 맞춥니다.
+                
+                노래 제출과 마찬가지로 한번 제출을 완료하면
+                다시 바꿀 수 없으니 주의하세요!
+                
+                튜토리얼 알쏭이는 얼마나 잘 불렀는지 확인해볼까요?
+                """
+            case .result:
+                """
+                노래, 허밍, 그리고 정답 제출까지 모두 익혔습니다!
+                
+                위 과정이 모두 끝나면 선택된 노래의 허밍
+                그리고 친구들의 정답을 확인하는 결과 화면이 시작됩니다.
+                
+                여러분의 허밍을 들은 알쏭이의 정답을 확인하러 가볼까요?
+                """
+            case .finished:
+                """
+                튜토리얼을 통해 "허밍" 모드를 익히셨습니다.
+                
+                다른 모드들도 준비되어 있으니 친구들과 함께
+                마음껏 실력을 발휘해 보세요!
+                
+                그럼 이제 본격적으로 시작해볼까요?
+                """
+        }
+    }
+
+    public var symbol: (systemName: String, color: String)? {
+        switch self {
+            case .lobby:
+                (systemName: "lightbulb.max", color: "FFCC00")
+            case .selectMusic:
+                (systemName: "music.note.list", color: "508DFD")
+            case .humming:
+                (systemName: "microphone", color: "FD5050")
+            case .submitAnswer:
+                (systemName: "music.note.list", color: "508DFD")
+            case .result:
+                nil
+            case .finished:
+                (systemName: "hand.thumbsup", color: "28A745")
+        }
+    }
+
+    public var topButton: (isHidden: Bool, imageName: String, text: String, backgroundColor: String) {
+        switch self {
+            case .lobby:
+                (isHidden: false, imageName: "play.fill", text: "튜토리얼 시작!", backgroundColor: "asYellow")
+            default:
+                (isHidden: true, imageName: "", text: "", backgroundColor: "")
+        }
+    }
+
+    public var bottomButton: (isHidden: Bool, imageName: String?, text: String, backgroundColor: String) {
+        switch self {
+            case .lobby:
+                (isHidden: false, imageName: "figure.play", text: "튜토리얼 탈출!", backgroundColor: "asMint")
+            default:
+                (isHidden: false, imageName: "play.fill", text: "시작하기!", backgroundColor: "asYellow")
+        }
+    }
+
+}
