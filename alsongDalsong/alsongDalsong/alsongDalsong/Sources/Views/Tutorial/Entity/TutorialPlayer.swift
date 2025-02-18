38b551aa2f648443b649a89ff7a68276c6bc5ed5
around-forest <kleedo19@naver.com>
2025-02-18 14:22:01 +0900

    [Feat] TutorialPlayer 객체 구현

---
+++ alsongDalsong/alsongDalsong/alsongDalsong/Sources/Views/Tutorial/Entity/TutorialPlayer.swift
@@ -0,0 +1,10 @@
+import ASEntity
+import Foundation
+
+struct TutorialPlayer {
+    var avatarURL: URL?
+    var selectedMusic: Music?
+    var hummingURL: URL?
+    var rehummingURL: URL?
+    var submittedMusic: Music?
+}
